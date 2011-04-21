40216a73e99993a7f1c52acf7ed982046d2311b1
Jens Wille <jens.wille@uni-koeln.de>
2011-04-21 23:08:22 +0200

    Refactoring bin/pru.
    Backwards-incompatible changes:
    - '-r' changed to '-e'
    - map defaults to 'true'
    - stop messing with $LOAD_PATH

--- spec/pru_spec.rb
+++ spec/pru_spec.rb
@@ -9,35 +9,35 @@ describe Pru do
   describe 'map' do
 
     it 'selects' do
-      %x{ls -l | ./bin/pru 'include?("G")'}.split($/).size.should == 2
+      %x{ls -l | #{PRU_CMD} 'include?("G")'}.split($/).size.should == 2
     end
 
     it 'selects via regex' do
-      %x{ls -l | ./bin/pru /G/}.split($/).size.should == 2
+      %x{ls -l | #{PRU_CMD} /G/}.split($/).size.should == 2
     end
 
     it 'selects via $.' do
-      %x{cat spec/test.txt | ./bin/pru '$.'}.split($/)[0...3].should == %w[1 2 3]
+      %x{cat spec/test.txt | #{PRU_CMD} '$.'}.split($/)[0...3].should == %w[1 2 3]
     end
 
     it 'maps' do
-      %x{echo abc | ./bin/pru 'gsub(/a/, "b")'}.should == "bbc\n"
+      %x{echo abc | #{PRU_CMD} 'gsub(/a/, "b")'}.should == "bbc\n"
     end
 
     it 'selects and reduces' do
-      %x{cat spec/test.txt | ./bin/pru 'include?("abc")' 'size'}.should == "3\n"
+      %x{cat spec/test.txt | #{PRU_CMD} 'include?("abc")' 'size'}.should == "3\n"
     end
 
     it 'opens files' do
-      %x{echo spec/test.txt | ./bin/pru 'File.read(self)'}.should == File.read('spec/test.txt')
+      %x{echo spec/test.txt | #{PRU_CMD} 'File.read(self)'}.should == File.read('spec/test.txt')
     end
 
     it 'open preserves whitespaces' do
-      %x{echo ' ab\tcd ' | ./bin/pru 'self'}.should == " ab\tcd \n"
+      %x{echo ' ab\tcd ' | #{PRU_CMD} 'self'}.should == " ab\tcd \n"
     end
 
     it 'works with continuous input' do
-      results = %x{ruby -e 'STDOUT.sync = true; puts 1; sleep 2; puts 1' | ./bin/pru 'Time.now.to_i'}.split($/)
+      results = %x{ruby -e 'STDOUT.sync = true; puts 1; sleep 2; puts 1' | #{PRU_CMD} 'Time.now.to_i'}.split($/)
       results.size.should == 2
       results.uniq.size.should == 2 # called at a different time -> parses as you go
     end
@@ -47,23 +47,23 @@ describe Pru do
   describe 'reduce' do
 
     it 'reduces' do
-      %x{cat spec/test.txt | ./bin/pru -r 'size'}.should == "5\n"
+      %x{cat spec/test.txt | #{PRU_CMD} -e 'size'}.should == "5\n"
     end
 
     it 'prints arrays as newlines' do
-      %x{cat spec/test.txt | ./bin/pru -r 'self'}.should == File.read('spec/test.txt')
+      %x{cat spec/test.txt | #{PRU_CMD} -e 'self'}.should == File.read('spec/test.txt')
     end
 
     it 'can sum' do
-      %x{cat spec/test.txt | ./bin/pru -r 'sum(&:to_i)'}.should == "1212\n"
+      %x{cat spec/test.txt | #{PRU_CMD} -e 'sum(&:to_i)'}.should == "1212\n"
     end
 
     it 'can mean' do
-      %x{cat spec/test.txt | ./bin/pru -r 'mean(&:to_i)'}.should == "242.4\n"
+      %x{cat spec/test.txt | #{PRU_CMD} -e 'mean(&:to_i)'}.should == "242.4\n"
     end
 
     it 'can grouped' do
-      %x{cat spec/test.txt | ./bin/pru -r 'grouped.map { |a, b| b.size }'}.should include("2\n")
+      %x{cat spec/test.txt | #{PRU_CMD} -e 'grouped.map { |a, b| b.size }'}.should include("2\n")
     end
 
   end
@@ -71,7 +71,7 @@ describe Pru do
   describe 'map and reduce' do
 
     it 'selects with empty string and reduces' do
-      %x{cat spec/test.txt | ./bin/pru '' 'size'}.should == "5\n"
+      %x{cat spec/test.txt | #{PRU_CMD} '' 'size'}.should == "5\n"
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+LIB_DIR = File.expand_path('../../lib', __FILE__)
+PRU_CMD = "#{File.join(File.dirname(LIB_DIR), 'bin', 'pru')} -I#{LIB_DIR}"
+
+$LOAD_PATH.unshift LIB_DIR
 require 'pru'
